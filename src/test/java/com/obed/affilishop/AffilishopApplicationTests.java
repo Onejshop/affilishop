dafb445c5b9f03df0a20e13883b2b135bb5043f8
Obed PAMY <obedpamy4322@gmail.com>
2026-01-30 15:08:30 -0500

    Deploy to Render

--- src/test/java/com/obed/affilishop/AffilishopApplicationTests.java
+++ src/test/java/com/obed/affilishop/AffilishopApplicationTests.java
@@ -2,12 +2,14 @@ package com.obed.affilishop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AffilishopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
